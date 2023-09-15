1fdad88a4d39a4f57e7708c50803eab1bf75fe45
Nuno <Mortumm@users.noreply.github.com>
2023-09-15 14:46:51 +0300

    routing update for Main

--- CsharpAlgo/BFS/Program.cs
+++ CsharpAlgo/BFS/Program.cs
@@ -1,25 +1,25 @@
 ﻿namespace Exercise004
 {
-    using System;
-    public class Program
+  using System;
+  public class Program
+  {
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            Labyrinth l = new Labyrinth();
-            char[,] c =
-            { {'#','#','#','#','#','#','#'},
+      Labyrinth l = new Labyrinth();
+      char[,] c =
+      { {'#','#','#','#','#','#','#'},
               {'#','x','#','.','.','.','#'},
               {'#','.','#','.','#','.','#'},
               {'#','.','.','.','.','y','#'},
               {'#','#','#','#','#','#','#'} };
-            Console.WriteLine(l.Search(c)); // DDRRUUR       
+      Console.WriteLine(l.Search(c)); // DDRRRRR       
 
-            Labyrinth l2 = new Labyrinth();
-            char[,] c2 =
-            { {'#','#','#','#','#','#'},
+      Labyrinth l2 = new Labyrinth();
+      char[,] c2 =
+      { {'#','#','#','#','#','#'},
               {'#','x','#','.','y','#'},
               {'#','#','#','#','#','#'} };
-            Console.WriteLine(l2.Search(c2)); // empty string 
-        }
+      Console.WriteLine(l2.Search(c2)); // empty string 
     }
+  }
 }
